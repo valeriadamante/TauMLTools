ebfa5d492cfff44e0d4b00d2342b347cd6811981
Valeria D'Amante <valeriadamante@gmail.com>
2021-04-01 10:05:58 +0200

    intermediate commit to change transofrmL2Tuple.py

--- Analysis/python/TransformL2Tuple.py
+++ Analysis/python/TransformL2Tuple.py
@@ -1,57 +1,56 @@
 import os
 import argparse
 
+parent_directory =  "/gpfs/ddn/srm/cms/store/user/vdamante/L2TauTuple/"
+parent_directory_2 = "/gpfs/ddn/srm/cms/store/user/vdamante/L2TausTuple/" # EphemeralHLTPhysics3 and ZprimeToTauTau
+files_directories = {
+"EphemeralHLTPhysics1": "crab_EphemeralHLTPhysics1",
+"EphemeralHLTPhysics2": "crab_EphemeralHLTPhysics2",
+"EphemeralHLTPhysics3": "crab_EphemeralHLTPhysics3",
+"EphemeralHLTPhysics4": "crab_EphemeralHLTPhysics4",
+"EphemeralHLTPhysics5": "crab_EphemeralHLTPhysics5",
+"EphemeralHLTPhysics6": "crab_EphemeralHLTPhysics6",
+"EphemeralHLTPhysics7": "crab_EphemeralHLTPhysics7",
+"EphemeralHLTPhysics8": "crab_EphemeralHLTPhysics8",
+"DYToLL_M-50_TuneCP5_14TeV-pythia8":"crab_DYToLL_M-50",
+"QCD_Pt_120to170_TuneCP5_14TeV_pythia8":"crab_QCD_Pt_120to170",
+"QCD_Pt-15to3000_TuneCP5_Flat_14TeV_pythia8":"crab_QCD_Pt-15to3000_Flat",
+"QCD_Pt-15to7000_TuneCP5_Flat_14TeV_pythia8":"crab_QCD_Pt-15to7000_Flat",
+"QCD_Pt-15to7000_TuneCP5_Flat_14TeV_pythia8":"crab_QCD_Pt-15to7000_Flat_ext1",
+"QCD_Pt_170to300_TuneCP5_14TeV_pythia8":"crab_QCD_Pt_170to300",
+"QCD_Pt_300to470_TuneCP5_14TeV_pythia8":"crab_QCD_Pt_300to470",
+"QCD_Pt_30to50_TuneCP5_14TeV_pythia8":"crab_QCD_Pt_30to50",
+"QCD_Pt_470to600_TuneCP5_14TeV_pythia8":"crab_QCD_Pt_470to600",
+"QCD_Pt_50to80_TuneCP5_14TeV_pythia8":"crab_QCD_Pt_50to80",
+"QCD_Pt_600oInf_TuneCP5_14TeV_pythia8":"crab_QCD_Pt_600oInf",
+"QCD_Pt_80to120_TuneCP5_14TeV_pythia8":"crab_QCD_Pt_80to120",
+"TTToSemiLeptonic_TuneCP5_14TeV-powheg-pythia8":"crab_TTToSemiLeptonic",
+"TT_TuneCP5_14TeV-powheg-pythia8":"crab_TT",
+"TT_TuneCP5_14TeV-powheg-pythia8":"crab_TT_ext1",
+"VBFHToTauTau_M125_TuneCUETP8M1_14TeV_powheg_pythia8":"crab_VBFHToTauTau",
+"WJetsToLNu_TuneCP5_14TeV-amcatnloFXFX-pythia8":"crab_WJetsToLNu",
+"ZprimeToTauTau_M-4000_TuneCP5_14TeV-pythia8-tauola":"crab_ZprimeToTauTau_M-4000",
+}
+
+
 
 parser = argparse.ArgumentParser()
 #parser.add_argument('machine', type=str, default="local", choices=["lxplus", "local"])
-parser.add_argument('--reference', required=True, type=str, help= "input reference file")
-parser.add_argument('--targets', required=True, type=str, help= "input target files to compare separated by comma")
-parser.add_argument('--variables', required=False, type=str, default= "", help= "input variables separated by comma")
-parser.add_argument('--txtname', required=False, type=str, default="all_paths", help= "txt file with all paths")
-parser.add_argument('--dirsuffix', required=False, type=str, default="", help="additional suffix for base directory name")
-parser.add_argument('--topdir', required=False, type=str, default="CMSSW_11_2_0_phase2", help="base directory name")
-parser.add_argument('--norm', required=False, type=bool, default=False, help="Draw normalized plots")
+parser.add_argument('--sample', required=True, type=str, help= "input reference file", choices=['DY', 'QCD', 'TT', 'VBF', 'WJets', 'ZPrime'])
+parser.add_argument('--n-threads', required=False, type=int, default=1, help='Number of threads')
+parser.add_argument('--start-entry', required=False, type=int, default=0 , help='start entry')
+parser.add_argument('--end-entry', required=False, type=int, default=-1, help='end entry')
+
 args = parser.parse_args()
 
-# ****** dictionary for in/out directories ******
-dir_dict = {}
-dir_dict["local"]={}
-#dir_dict["local"]["input"]="/Users/valeriadamante/Desktop/Dottorato/public/CMSSW_11_2_0_pre9/src/RootFiles/"
-dir_dict["local"]["input"] = "/Users/valeriadamante/Desktop/Dottorato/public/CMSSW_11_3_0_pre4/src/Validate_files/output/"
-dir_dict["local"]["output"] = "/Users/valeriadamante/Desktop/Dottorato/public/CMSSW_11_3_0_pre4/src/ValidationPlots/"
-#dir_dict["local"]["output"]="/Users/valeriadamante/Desktop/Dottorato/public/CMSSW_11_2_0_pre9/src/"
-
-dir_dict["lxplus"]={}
-dir_dict["lxplus"]["input"]="/afs/cern.ch/work/v/vdamante/public/CMSSW_11_3_0_pre4/src/Validate_files/output/"
-dir_dict["lxplus"]["output"]="/afs/cern.ch/work/v/vdamante/public/CMSSW_11_3_0_pre4/src/ValidationPlots/"#"/eos/home-v/vdamante/www/phase2validation/"
-
-input_dir = dir_dict[args.machine]["input"]
-out_dir =  dir_dict[args.machine]['output']
-
-# **** create txt file with all paths  ****
-input_files_str = args.reference+","+args.targets
-if not os.path.exists(args.txtname+".txt"):
-    command="root -l -b make_histogram.cpp+O\(\\\""+input_files_str+"\\\",\\\""+args.txtname+"\\\"\)"
-    print command
-    os.system(command)
-
-# **** create list of files ****
-input_files = []
-input_files.append(input_dir+args.reference)
-for file in (input_dir+args.targets).split(","):
-    input_files.append(file)
-
-# **** initialize the class and its values ****
-make_histogram = histomaker(input_files)
-make_histogram.file_name=args.txtname+".txt"                            # file name with paths
-make_histogram.top_level_dir = out_dir+args.topdir+args.dirsuffix       # base directory name
-make_histogram.norm=args.norm
-# **** eventually initialize variables ****
-variables =[]
-if args.variables != "":
-    for var in (args.variables).split(","):
-        variables.append(var)
-    #print variables
-    make_histogram.variables = variables
-
-# **** draw histograms ****
+cmd = 'L2TrainingTupleProducer --input "VBFHToTauTau_M125_TuneCUETP8M1_14TeV_powheg_pythia8_output1.root" --output "VBFProvaoutput_2.root" --isQCD false'
+inputs =[]
+for k in files_directories:
+    if(args.sample in k):
+        parent = parent_directory
+        if(k == "ZprimeToTauTau_M-4000_TuneCP5_14TeV-pythia8-tauola" or k == "EphemeralHLTPhysics3" ):
+            parent = parent_directory_2
+        path = parent + k + "/"+files_directories[k]+"/"
+        for file in os.listdir( path ):
+            print path+file
+            #inputs.append()
